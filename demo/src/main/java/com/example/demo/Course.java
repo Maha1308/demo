bdc12f038b1f922710c7400739bfe3a0a888fd73
MAHA <MAHA@DESKTOP-GUFMCJ1>
2022-05-29 14:11:29 +0530

    commit

---
+++ demo/src/main/java/com/example/demo/Course.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Course {
+	static void courseDisplay()
+	{
+		System.out.println("java full Stack Developer");
+	}
+
+	
+
+}
